aef8c5f899851aeb6fbae0e13c51121f027eb5f1
leehyuna <leehyuna77@naver.com>
2022-07-21 01:03:27 +0900

    first commit

---
+++ ijoy/src/main/java/com/d202/ijoy/IjoyApplication.java
@@ -0,0 +1,13 @@
+package com.d202.ijoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjoyApplication.class, args);
+	}
+
+}
---
+++ ijoy/src/test/java/com/d202/ijoy/IjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d202.ijoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
